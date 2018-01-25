f0aa995ee104d69394e0a1ea2d02de592afaca68
Zach Fudge <zacheryfudge@gmail.com>
2018-01-25 09:21:00 -0700

    fixed splitter function bug, conversion returning correct conversion, sets surrounding values without arguments

--- roman.js
+++ roman.js
@@ -8,45 +8,43 @@ const roman = {
 	"1000": "M"
 };
 
-const split = (num) => {
+const splitter = (num) => {
 	return (num <= 10) ? [num] : (
-		(num <= 100) ? (num % 10 === 0) ? num : [num - (num % 10), num % 10]
-		: (num <= 1000) ? (num % 10 === 0) ? num : [num - (num % 100), (num % 100) - (num % 10), num % 10]
-		: (num % 10 === 0) ? num : [num - (num % 1000), (num % 1000) - (num % 100), (num % 100) - (num % 10), num % 10]
+		(num <= 100) ? (num % 10 === 0) ? [num] : [num - (num % 10), num % 10]
+		: (num <= 1000) ? (num % 10 === 0) ? [num] : [num - (num % 100), (num % 100) - (num % 10), num % 10]
+		: (num % 10 === 0) ? [num] : [num - (num % 1000), (num % 1000) - (num % 100), (num % 100) - (num % 10), num % 10]
 	);
 };
 
-function convert(arr) {
-	const newArr = [];
+function convertToRoman(num) {
+	const newArray = [];
+	const split = splitter(num);
 
+	split.forEach(function(cur) {
+		newArray.push(conversion(cur));
+	});
 
-
-	return newArr.join("");
+	num = newArray.join("");
+	return num;
 }
 
-function conversion(single, half, whole, num) {
+function conversion(num) {
+	let single,half,whole;
+	(num <= 10) ? single = 1 : (num <= 100) ? single = 10 : (num <= 1000) ? single = 100 : single = 1000;
+	half = single * 5;
+	whole = half * 2;
 	const newArr = [];
-
 	if (whole === num) {
-		newArr.push(whole);
+		newArr.push(roman[`${whole}`]);
 	} else if (num === whole * 0.4 || num === whole * 0.9) {
-		if (num < half) {
-			newArr.push(single,half);
-		} else {
-			newArr.push(single,whole);
-		}
+		(num < half) ? newArr.push(roman[`${single}`],roman[`${half}`]) : newArr.push(roman[`${single}`],roman[`${whole}`]);
 	} else {
 		if (num % half === num) {
-			for (let i = single; i <= num; i += single) {
-				newArr.push(single);
-			}
+			for (let i = single; i <= num; i += single) newArr.push(roman[`${single}`]);
 		} else {
-			newArr.push(half);
-			for (let i = single; i <= num % half; i += single) {
-				newArr.push(single);
-			}
+			newArr.push(roman[`${half}`]);
+			for (let i = single; i <= num % half; i += single) newArr.push(roman[`${single}`]);
 		}
 	}
-
 	return newArr.join("");
 }
